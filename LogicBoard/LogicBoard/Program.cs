d2a2a572c21dc5d41a050c493749dbb6e922dde2
FilipMargitai <filip.margitai@pslib.cz>
2021-02-23 21:16:50 +0100

    solution added

---
+++ LogicBoard/LogicBoard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogicBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
